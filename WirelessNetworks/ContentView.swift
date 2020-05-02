b3c8ccba9864c590121ddac1f3e489c8e897679e
Salihcan Kahya <salihcankahya@gmail.com>
2020-05-02 23:07:57 +0300

    Initial Commit

---
+++ WirelessNetworks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WirelessNetworks
+//
+//  Created by Salihcan Kahya on 2.05.2020.
+//  Copyright © 2020 Salihcan Kahya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
